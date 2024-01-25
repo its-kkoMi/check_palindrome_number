8a46d3410cb362d65cd1337701756ea63de9aa96
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 05:43:52 +0800

    Program Instructions and Pseudocode

---
+++ check_palindrome_number.py
@@ -0,0 +1,7 @@
+# Assignment 5 - Exercise 9
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers.
+
+# Ask user for a number
+# Check if number is a palindrome
+# Print yes or no if number is palindrome
